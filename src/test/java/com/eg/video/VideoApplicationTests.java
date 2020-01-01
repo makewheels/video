0b9a722fb7b651611881e31ef7cf22760287954e
Administrator <user@email.com>
2020-01-01 21:38:59 +0800

    项目创建

---
+++ src/test/java/com/eg/video/VideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
